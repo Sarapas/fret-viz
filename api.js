dc30027997fea1d3bee966421d3db6fdf78f0d5d
Justas Sarapas <justas.sarapas@gmail.com>
2020-04-10 16:27:19 +0300

    showing C major scale correctly

--- api.js
+++ api.js
@@ -3,12 +3,32 @@ const canvas = createCanvas(1942, 372)
 const ctx = canvas.getContext('2d')
 const fs = require('fs')
 
+var E = 0;
+var F = 1;
+var Gb = 2;
+var G = 3;
+var Ab = 4;
+var A = 5;
+var Bb = 6;
+var B = 7;
+var C = 8;
+var Db = 9;
+var D = 10;
+var Eb = 11;
+
+var chromatic = [ E, F, Gb, G, Ab, A, Bb, B, C, Db, D, Eb, E ];
+
+var intervals = [ "R", "m2", "2", "m3", "3", "P4", "4#", "P5", "m6", "6", "m7", "7", "R" ];
+var names = [ "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#", "D", "D#", "E" ];
+var tuning = [ E, A, D, G, B, E ];
+var display = [ C, D, E, F, G, A, B ];
+
 var stringTop1 = 92;
 var stringBottom1 = 286;
 var stringTop12 = 66;
 var stringBottom12 = 310;
 
-var frets = [ 162, 200, 190, 178, 170, 158, 152, 137, 134, 122, 120, 112]
+var frets = [ 20, 142, 200, 190, 178, 170, 158, 152, 137, 134, 122, 120, 112]
 
 var drawNote = (ctx, fret, string, isRoot, text) => {
   var black = "#000000";
@@ -48,12 +68,34 @@ var drawNote = (ctx, fret, string, isRoot, text) => {
   ctx.fillText(text, textX, textY)
 }
 
+var getNote = (tuning, fret, string) => {
+  var note = tuning[5 - string] + fret;
+  if (note >= 12) note -= 12;
+  return note;
+}
+
+var getNoteInterval = (root, note) => {
+  var n = note - root;
+  if (n < 0) n += 12;
+  return intervals[n];
+};
+
+var getNoteName = (note) => {
+  return names[note];
+}
+
 loadImage('./src/assets/fretboard-large.png').then((image) => {
   ctx.drawImage(image, 0, 0);
 
-  for (var fret = 0; fret < 12; fret++) {
+  for (var fret = 0; fret < 13; fret++) {
     for (var string = 0; string < 6; string++) {
-      drawNote(ctx, fret, string, false, 'M7');
+      var note = getNote(tuning, fret, string);
+      if (display.indexOf(note) > -1) {
+        var root = display[0];
+        // var text = getNoteInterval(root, note);
+        var text = getNoteName(note);
+        drawNote(ctx, fret, string, root === note, text);
+      }
     }
   }
 
